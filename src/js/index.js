bbcc96d48a00f8f6ec958204af5b0575fb69a974
vasylbvb <basebv@gmail.com>
2019-04-10 23:22:23 +0300

    header HTML/CSS start

--- src/js/index.js
+++ src/js/index.js
@@ -3,5 +3,29 @@ import popper from 'popper.js';
 import bootstrap from 'bootstrap';
 
 jQuery(function($) {
+    //Scrollspy for changing header
+    window.onscroll = function() { listenToScroll() };
+    const header = $('header.main-nav');
+    const sticky = header.scrollTop() + 70;
 
+    function listenToScroll() {
+        if (window.pageYOffset > sticky) {
+            header.addClass("scrolled");
+        } else {
+            header.removeClass("scrolled");
+        }
+    }
+
+    //Header Location Dropdown
+    var locationArr = $(".navbar-location .dropdown-item");
+    $(locationArr).each(function() {
+        $(this).text($(this).data("name"));
+    });
+
+    $(locationArr).click(function(e) {
+        e.preventDefault();
+        $(locationArr).removeClass("active");
+        $(this).addClass("active");
+        $("#dropdownLocBtn").find(".text").text($(this).text());
+    });
 });
