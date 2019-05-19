47006341007cb8b249d3ba6523e66fd95995bb4c
vasylbvb <basebv@gmail.com>
2019-05-19 08:52:58 +0300

    CSS fixes, success order modal, toggling mobile steps and footer, header reconstructed

--- src/js/index.js
+++ src/js/index.js
@@ -10,10 +10,14 @@ import "jquery-ui/ui/i18n/datepicker-uk";
 import locations from "../js/dbs/locations.json";
 import services from "../js/dbs/services.json";
 
-jQuery(function($) {
+jQuery(function ($) {
     //Scrollspy for changing header
-    window.onload = function() { listenToScroll() };
-    window.onscroll = function() { listenToScroll() };
+    window.onload = function () {
+        listenToScroll()
+    };
+    window.onscroll = function () {
+        listenToScroll()
+    };
     const header = $('header.main-nav');
     const sticky = header.scrollTop() + 70;
 
@@ -28,11 +32,11 @@ jQuery(function($) {
     //Header Location Dropdown
 
     /*$('#locModal').on('hide.bs.modal', function (e) {
-        var getLocInpVal = $(this).find('input[type="text"]').val();
-        if ( getLocInpVal != 0 ) {
-            $(".navbar-location__btn").find(".text").text(getLocInpVal);
-        }
-    });*/
+     var getLocInpVal = $(this).find('input[type="text"]').val();
+     if ( getLocInpVal != 0 ) {
+     $(".navbar-location__btn").find(".text").text(getLocInpVal);
+     }
+     });*/
     // var locationArr = $("#locModal .dropdown-item");
     // $(locationArr).each(function() {
     //     $(this).text($(this).data("name"));
@@ -49,7 +53,7 @@ jQuery(function($) {
     //Header Language Dropdown
     var languageArr = $(".navbar-lang .dropdown-item");
 
-    $(languageArr).click(function(e) {
+    $(languageArr).click(function (e) {
         e.preventDefault();
         $(languageArr).removeClass("active");
         $(this).addClass("active");
@@ -59,22 +63,22 @@ jQuery(function($) {
 
     //Location input autocomplete functions start
     // Highlight matched text
-    $.widget( "app.autocomplete", $.ui.autocomplete, {
+    $.widget("app.autocomplete", $.ui.autocomplete, {
 
         // Which class get's applied to matched text in the menu items.
 
-        _renderItem: function( ul, item ) {
+        _renderItem: function (ul, item) {
 
             // Replace the matched text with a custom span. This
             // span uses the class found in the "highlightClass" option.
-            var re = new RegExp( "(" + this.term + ")", "gi" ),
+            var re = new RegExp("(" + this.term + ")", "gi"),
                 cls = this.options.highlightClass,
                 template = "<span class='" + cls + "'>$1</span>",
-                label = item.label.replace( re, template ),
-                $li = $( "<li/>" ).appendTo( ul );
+                label = item.label.replace(re, template),
+                $li = $("<li/>").appendTo(ul);
 
             // Create and return the custom menu item content.
-            $( "<span/>" ).html( label ).appendTo( $li );
+            $("<span/>").html(label).appendTo($li);
 
             return $li;
 
@@ -82,11 +86,11 @@ jQuery(function($) {
     });
 
     function searchInpFunc(input) {
-        $(input).on("input focus", function(e) {
+        $(input).on("input focus", function (e) {
             var clearBtn = $(this).siblings(".clear-input");
-            if($(this).val() != 0) {
+            if ($(this).val() != 0) {
                 $(clearBtn).show();
-                $(clearBtn).click(function(e) {
+                $(clearBtn).click(function (e) {
                     e.preventDefault();
                     $(this).siblings(input).val('');
                     $(this).hide();
@@ -96,7 +100,7 @@ jQuery(function($) {
             }
         });
 
-        $(input).parents(".region-search").on("blur", function(e) {
+        $(input).parents(".region-search").on("blur", function (e) {
             $(this).find(".clear-input").hide();
         });
     }
@@ -111,13 +115,13 @@ jQuery(function($) {
     $(myInpNav).autocomplete({
         source: locations.locations,
         highlightClass: "auto-matches",
-        open: function() {
-            $("ul.ui-menu").width( $(this).innerWidth() );
+        open: function () {
+            $("ul.ui-menu").width($(this).innerWidth());
         },
-        select: function( event, ui ) {
+        select: function (event, ui) {
             var checkInpVal = $(myInpNav).val(ui.item.value);
             $(myInpNav).attr("value", ui.item.value);
-            if (  $(checkInpVal) != 0 ) {
+            if ($(checkInpVal) != 0) {
                 $(".navbar-location__btn").find(".text").html(ui.item.value);
             }
             $("#contactModal").find("#s-order-loc").val(ui.item.value).attr("value", ui.item.value);
@@ -129,10 +133,10 @@ jQuery(function($) {
     $(myInp).autocomplete({
         source: locations.locations,
         highlightClass: "auto-matches",
-        open: function() {
-            $("ul.ui-menu").width( $(this).innerWidth() );
+        open: function () {
+            $("ul.ui-menu").width($(this).innerWidth());
         },
-        select: function( event, ui ) {
+        select: function (event, ui) {
             var checkInpVal = $(myInp).val(ui.item.value);
             $(myInp).attr("value", ui.item.value);
             $("#contactModal").find("#s-order-loc").val(ui.item.value).attr("value", ui.item.value);
@@ -143,10 +147,10 @@ jQuery(function($) {
     $(myInpOrder).autocomplete({
         source: locations.locations,
         highlightClass: "auto-matches",
-        open: function() {
-            $("ul.ui-menu").width( $(this).innerWidth() );
+        open: function () {
+            $("ul.ui-menu").width($(this).innerWidth());
         },
-        select: function( event, ui ) {
+        select: function (event, ui) {
             var checkInpVal = $(myInpOrder).val(ui.item.value);
             $(myInpOrder).attr("value", ui.item.value);
         }
@@ -155,10 +159,10 @@ jQuery(function($) {
     $(searchServ).autocomplete({
         source: services.services,
         highlightClass: "auto-matches",
-        open: function() {
-            $("ul.ui-menu").width( $(this).innerWidth() );
+        open: function () {
+            $("ul.ui-menu").width($(this).innerWidth());
         },
-        select: function( event, ui ) {
+        select: function (event, ui) {
             $(searchServ).val(ui.item.value);
             $(searchServ).attr("value", ui.item.value);
             $("#contactModal").find("#chosen-service").val(ui.item.value).attr("value", ui.item.value);
@@ -170,10 +174,10 @@ jQuery(function($) {
     $(searchServBig).autocomplete({
         source: services.services,
         highlightClass: "auto-matches",
-        open: function() {
-            $("ul.ui-menu").width( $(this).innerWidth() );
+        open: function () {
+            $("ul.ui-menu").width($(this).innerWidth());
         },
-        select: function( event, ui ) {
+        select: function (event, ui) {
             $(searchServBig).val(ui.item.value);
             $(searchServBig).attr("value", ui.item.value);
             $("#contactModal").find("#chosen-service").val(ui.item.value).attr("value", ui.item.value);
@@ -189,33 +193,62 @@ jQuery(function($) {
 
     //toggle services sublists in services modal
     var servExpand = $(".modal-serv .has-children .text");
-    $(servExpand).click(function(e) {
+    $(servExpand).click(function (e) {
         e.preventDefault();
-       $(this).parents(".has-children").toggleClass("opened");
+        $(this).parents(".has-children").toggleClass("opened");
     });
 
     //slide modal columns on mobile
     var servTabLinks = $(".modal-serv .nav-tabs .nav-link");
-    $(servTabLinks).click(function() {
+    $(servTabLinks).click(function () {
         $(this).parents(".row").addClass("slided");
     });
 
     var servBackBtn = $(".modal-serv__back-btn");
-    $(servBackBtn).click(function() {
+    $(servBackBtn).click(function () {
         $(this).parents(".row").removeClass("slided");
     });
 
     //toggle region lists
     var listHasChild = $(".block-regions li.has-children");
-    $(listHasChild).find(".text").click(function() {
+    $(listHasChild).find(".text").click(function () {
+        $(listHasChild).find(".text").not($(this)).parents(".has-children").removeClass('opened');
         $(this).parents(".has-children").toggleClass("opened");
     });
 
     //hero block badges functionality
-    $(".hero-inner__tags-line button").click(function(e) {
+    $(".hero-inner__tags-line button").click(function (e) {
         e.preventDefault();
         var badgeText = $(this).text();
         $("#contactModal").find("#chosen-service").val(badgeText).attr("value", badgeText);
+        $("#contactModal").modal("show");
+    });
+
+    //services links modal functionality
+    var servModalLinks = $(".modal-serv__list--common > li").not(".has-children").find("a");
+    $(servModalLinks).click(function (e) {
+        $("#contactModal").find("#chosen-service").val($(this).text()).attr("value", $(this).text());
+        $("#servicesModal").modal("hide");
+        $("#contactModal").modal("show");
+    });
+
+    var servModalLinks2 = $(".modal-serv__list--common > .has-children .sublist").find("a");
+    $(servModalLinks2).click(function (e) {
+        $("#contactModal").find("#chosen-service").val($(this).text()).attr("value", $(this).text());
+        $("#servicesModal").modal("hide");
+        $("#contactModal").modal("show");
+    });
+
+    //regions links functionality
+    var regionLinks = $(".block-regions__list--main .sublist li").find("a");
+    $(regionLinks).click(function (e) {
+        $("#contactModal").find("#s-order-loc").val($(this).text()).attr("value", $(this).text());
+        $("#contactModal").modal("show");
+    });
+
+    var regionLinks2 = $(".block-regions__list--common .sublist li").find("a");
+    $(regionLinks2).click(function (e) {
+        $("#contactModal").find("#s-order-loc").val($(this).text()).attr("value", $(this).text());
         $("#contactModal").modal("show");
     });
 
@@ -230,14 +263,14 @@ jQuery(function($) {
     $('[data-toggle="tooltip"]').tooltip();
 
     //Initialize datepicker
-    $("#client-date").datepicker( $.datepicker.regional[ "uk" ] ).datepicker( "option", "minDate", new Date() );
+    $("#client-date").datepicker($.datepicker.regional["uk"]).datepicker("option", "minDate", new Date());
 
     /*Contact form validation START*/
-    $('#contactModal [type="submit"]').click(function(e){
+    $('#contactModal [type="submit"]').click(function (e) {
         e.preventDefault();
 
         var nameReg = /^[A-Za-z\u0400-\u04FF]+$/;
-        var numberReg =  /^[0-9]+$/;
+        var numberReg = /^[0-9]+$/;
         //var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
 
         var clientName = $('#client-name');
@@ -263,10 +296,10 @@ jQuery(function($) {
         //     $(inputs[0]).parents(".form-group").addClass("has-error").append(errMsg);
         // }
 
-        if(inputs[1].val() == ""){
+        if (inputs[1].val() == "") {
             var errMsg = $('<span></span>').addClass("error").text(inputMessage[0]);
             $(inputs[1]).parents(".form-group").addClass("has-error").append(errMsg);
-        } else if(!numberReg.test(inputs[1].val())){
+        } else if (!numberReg.test(inputs[1].val())) {
             var errMsg = $('<span></span>').addClass("error").text(inputMessage[0]);
             $(inputs[1]).parents(".form-group").addClass("has-error").append(errMsg);
         }
@@ -286,19 +319,59 @@ jQuery(function($) {
         //     $(inputs[4]).parents(".form-group").addClass("has-error").append(errMsg);
         // }
 
-        if( $('#contactModal .error').length == 0 ) {
-            $('#contactModal form')[0].submit();
+        if ($('#contactModal .error').length == 0) {
+            //$('#contactModal form')[0].submit(); /*activate in production*/
+            $("#contactModal").modal('hide');
+            /*deactivate in production*/
+            $("#successModal").modal('show');
         } else {
             return false;
         }
     });
     /*Contact form validation END*/
 
+    /*Success modal START*/
+    $("#successModal").on('show.bs.modal', function () {
+        var infoName = $("#infoName"),
+            infoLoc = $("#infoLoc"),
+            infoTime = $("#infoTime"),
+            infoDate = $("#infoDate"),
+            infoPhone = $("#infoPhone"),
+            clientName = $("#client-name").val(),
+            clientLoc = $("#s-order-loc").attr('value'),
+            clientTime = $("#client-time").val(),
+            clientDate = $("#client-date").val(),
+            clientPhone = $("#client-phone").val();
+
+        if (clientName != "") {
+            $(infoName).html(clientName + ', ');
+        }
+
+        if ($(clientLoc).end().length >= 1) {
+            $(infoLoc).html(' в ' + clientLoc);
+        }
+
+        if (clientTime != "") {
+            $(infoTime).html(' на ' + clientTime);
+        }
+
+        if (clientDate != "") {
+            $(infoDate).html(' ' + clientDate);
+        }
+
+        if (clientPhone != "") {
+            $(infoPhone).html('Ваш телефон ' + clientPhone + '.');
+        }
+
+        console.log($(clientLoc));
+    });
+    /*Success modal END*/
+
     /*Sliders start*/
     //orders slider
     var orderEther = $("#orderEther"),
         counterEther = $(".order-slider__nav-counter");
-    $(orderEther).on('init reInit afterChange', function(event, slick, currentSlide, nextSlide){
+    $(orderEther).on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
         //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
         var i = (currentSlide ? currentSlide : 0) + 1;
         $(counterEther).html('<span class="m-text-red">' + i + '</span>/' + slick.slideCount);
@@ -307,7 +380,7 @@ jQuery(function($) {
     $(orderEther).slick({
 
         infinite: true,
-        slidesToShow: 2.5,
+        slidesToShow: 3,
         autoplay: false,
         arrows: true,
         prevArrow: '.order-slider__nav-prev',
@@ -338,7 +411,7 @@ jQuery(function($) {
     //feedback slider
     var feedbackSlider = $("#feedbackSlider"),
         counterFeedbacks = $(".block-feedback__slider-nav-counter");
-    $(feedbackSlider).on('init reInit afterChange', function(event, slick, currentSlide, nextSlide){
+    $(feedbackSlider).on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
         //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
         var i = (currentSlide ? currentSlide : 0) + 1;
         $(counterFeedbacks).html('<span class="m-text-red">' + i + '</span>/' + slick.slideCount);
@@ -357,7 +430,7 @@ jQuery(function($) {
     /*Sliders end*/
 
     //Video modal Start
-    $(".block-feedback__decor-btn.video").click(function() {
+    $(".block-feedback__decor-btn.video").click(function () {
         var getSrc = $(this).attr("data-link");
         $("#videoModal").on('show.bs.modal', function () {
             $(this).find("iframe").attr("src", getSrc);
@@ -369,20 +442,38 @@ jQuery(function($) {
     });
     //Video modal End
 
+    //Audio modal Start
+    $(".block-feedback__decor-btn.audio").click(function () {
+        var getSrc = $(this).attr("data-link");
+        $("#audioModal").on('show.bs.modal', function () {
+            $(this).find("source").attr("src", getSrc);
+        });
+    });
+
+    $("#audioModal").on('hide.bs.modal', function () {
+        $(this).find("source").removeAttr("src");
+    });
+    //Audio modal End
+
 
     //footer menus toggle
     var $width = $(window).width();
 
     if ($width < 992) {
         var footerTitles = $(".footer-menu__title");
-        $(footerTitles).click(function(e) {
-           e.preventDefault();
+        $(footerTitles).click(function (e) {
+            e.preventDefault();
             $(this).parent(".footer-menu").toggleClass("opened");
+            if($(this).parent(".footer-menu").hasClass("opened")) {
+                $(this).siblings(".footer-menu__list").fadeIn().css("display", "flex");
+            } else {
+                $(this).siblings(".footer-menu__list").fadeOut();
+            }
         });
 
         /*Service steps mobile expanding*/
         var serviseSteps = $(".block-order-steps__step");
-        $(serviseSteps).click(function(e) {
+        $(serviseSteps).click(function (e) {
             e.preventDefault();
             $(serviseSteps).removeClass("opened");
             $(this).addClass("opened");
@@ -390,23 +481,34 @@ jQuery(function($) {
 
         /*Advantages steps mobile expanding*/
         var advItems = $(".block-advantages__list-item");
-        $(advItems).click(function(e) {
+        $(advItems).click(function (e) {
             e.preventDefault();
             //$(advItems).removeClass("opened");
             $(this).toggleClass("opened");
         });
     }
 
-    $(window).resize(function(){
+    $(window).resize(function () {
         var $width = $(window).width();
         if ($width < 992) {
-            var footerTitles = $(".footer-menu__title");
-            var footerMenus = $(".footer-menu");
-            $(footerMenus).removeClass("opened");
-            $(footerTitles).click(function(e) {
-                e.preventDefault();
-                $(this).parent(".footer-menu").toggleClass("opened");
-            });
+            // var footerTitles = $(".footer-menu__title");
+            // var footerMenus = $(".footer-menu");
+            // $(footerMenus).removeClass("opened");
+            // $(footerTitles).click(function(e) {
+            //     e.preventDefault();
+            //     $(this).parent(".footer-menu").toggleClass("opened");
+            // });
+        }
+    });
+
+    $(document).click(function (e) {
+        // if the target of the click isn't the container nor a descendant of the container
+        // close main menu modal on aside click
+        var mMenuBody = $('.main-menu .modal-body'),
+            mMenuFooter = $('.main-menu .modal-footer');
+
+        if (!mMenuBody.is(e.target) && mMenuBody.has(e.target).length === 0 && !mMenuFooter.is(e.target) && mMenuFooter.has(e.target).length === 0) {
+            $('.modal.main-menu').modal('hide');
         }
     });
 });
